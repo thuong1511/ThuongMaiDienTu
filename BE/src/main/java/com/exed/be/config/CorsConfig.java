a63f76e7bbe3b12ab2e6cac6500e8043b732c4f9
Thuong <nguyenthithuong15112005@gmail.com>
2026-05-16 17:38:58 +0700

    fix upload ảnh

--- BE/src/main/java/com/exed/be/config/CorsConfig.java
+++ BE/src/main/java/com/exed/be/config/CorsConfig.java
@@ -31,7 +31,21 @@ public class CorsConfig implements WebMvcConfigurer {
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Serve ảnh tĩnh từ GiaodienWeb/images/ (nằm cùng cấp với thư mục BE/)
+        // Dùng đường dẫn tuyệt đối để tránh nhầm working directory
+        String imagesPath = "file:" + java.nio.file.Paths.get(System.getProperty("user.dir"))
+                .getParent()
+                .resolve("GiaodienWeb/images/")
+                .toAbsolutePath()
+                .toString()
+                .replace("\\", "/") + "/";
+        System.out.println("[CorsConfig] Serving /images/** from: " + imagesPath);
+
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:GiaodienWeb/images/");
+                .addResourceLocations(imagesPath);
+
+        // Serve ảnh upload từ uploads/ (tương đối với thư mục BE/)
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
     }
 }
